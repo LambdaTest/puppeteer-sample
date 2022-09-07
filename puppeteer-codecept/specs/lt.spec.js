4af0b1df9d0d7401fc4cb38acca15d9726e95fe5
Mukesh Rana <mukeshr@lambdatest.com>
2022-09-07 16:04:05 +0530

    adding puppeteer-codecept sample

---
+++ puppeteer-codecept/specs/lt.spec.js
@@ -0,0 +1,38 @@
+Feature('Search Duckduckgo');
+
+    Before(({ I }) => { // or Background
+        I.amOnPage('https://www.bing.com');
+    });
+  
+    Scenario('should be titled "LambdaTest"', async ({ I }) => {
+        const text = 'LambdaTest';
+        I.amOnPage('https://www.duckduckgo.com');
+        I.click('[name="q"]');
+        I.fillField('[name="q"]', text);
+        I.pressKey('Enter');
+        I.seeInTitle(text + ' at DuckDuckGo');
+        let title = await I.grabTitle();
+        if(title.includes(text + ' at DuckDuckGo')) {
+            I.usePuppeteerTo('set test status', async ({ page, browser }) => {
+                await page.evaluate(
+                    (_) => {},
+                    `lambdatest_action: ${JSON.stringify({
+                        action    : 'setTestStatus',
+                        arguments : { status: 'passed', remark: 'assertion passed' }
+                    })}`
+                );
+            });
+        } else {
+            I.usePuppeteerTo('set test status', async ({ page, browser }) => {
+                await page.evaluate(
+                    (_) => {},
+                    `lambdatest_action: ${JSON.stringify({
+                        action    : 'setTestStatus',
+                        arguments : { status: 'failed', remark: 'assertion failed' }
+                    })}`
+                );
+            });
+        }
+    });
+
+
